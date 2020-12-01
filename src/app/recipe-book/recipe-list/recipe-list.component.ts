d0dbfd96df0fe9534c4873b08745bdcb861cf499
Casey <casey.gardonio@foat.org>
2020-11-30 20:02:55 -0600

    completed through 3-53

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('This is a test recipe', 'This is only a test', '../../assets/SwedishChef.gif'),
+    new Recipe('This is a test recipe', 'This is only a test', '../../assets/SwedishChef.gif'),
+    new Recipe('This is a test recipe', 'This is only a test', '../../assets/SwedishChef.gif'),
+    
+  ];
 
   constructor() { }
 

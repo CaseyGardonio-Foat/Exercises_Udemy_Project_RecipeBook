382de670d7bdeff4f004d1b809e7e365b29b9f9a
Casey <casey.gardonio@foat.org>
2020-12-13 00:11:01 -0600

    first commit for Udemy Section 8--generated new directive using CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { Dropdown } from './shared/dropdown.directive.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recip
     RecipeBookComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    Dropdown.DirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dropdown.directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { Dropdown.DirectiveDirective } from './dropdown.directive.directive';
+
+describe('Dropdown.DirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new Dropdown.DirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});

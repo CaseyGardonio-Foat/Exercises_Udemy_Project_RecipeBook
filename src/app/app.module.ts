83c7aecc66aa0b93493ecf313cd8b0d672a4c7b1
Casey <casey.gardonio@foat.org>
2020-12-13 00:12:52 -0600

    removed CLI-generated directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { Dropdown } from './shared/dropdown.directive.directive';
 
 @NgModule({
   declarations: [
--- src/app/shared/dropdown.directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Dropdown.DirectiveDirective } from './dropdown.directive.directive';
-
-describe('Dropdown.DirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new Dropdown.DirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});

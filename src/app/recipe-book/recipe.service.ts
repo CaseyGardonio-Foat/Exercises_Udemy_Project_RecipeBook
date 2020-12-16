0608047b95ab7018b1f6e0f7ba4873dc64528211
Casey <casey.gardonio@foat.org>
2020-12-16 16:39:07 -0600

    Class 5 Exercises: Udemy Section 10.118

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,6 +1,9 @@
-import { Recipe } from './recipe.model'
+import { Recipe } from './recipe.model';
+import { Output, EventEmitter } from '@angular/core'
 
 export class RecipeService {
+    @Output() recipeSelected = new EventEmitter<Recipe>()
+
     private recipes: Recipe[] = [
         new Recipe('This is a test recipe', 'This is only a test', '../../assets/SwedishChef.gif'),
         new Recipe('This is another test recipe', 'This is only a test', '../../assets/SwedishChef.gif'),

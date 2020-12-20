4de07a3bf685e3600caddc92c81d0014723c1ba7
Casey <casey.gardonio@foat.org>
2020-12-20 11:17:51 -0600

    fixed problem VSC showing in shopping-list.service.ts; all other tasks from Section 10 complete

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,7 @@ import { Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model'
 
 export class ShoppingListService {
-    @Output() ingredientsUpdated = new EventEmitter<Ingredient[]>()
+    ingredientsUpdated = new EventEmitter<Ingredient[]>()
     ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),

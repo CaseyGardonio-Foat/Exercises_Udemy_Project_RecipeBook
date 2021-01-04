c61b59831667180427a4bf56414620e412e87421
Casey <casey.gardonio@foat.org>
2021-01-04 13:41:33 -0600

    Section 12-part 156, unidentified bug with *ngFor and *ngIf in child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component'
 

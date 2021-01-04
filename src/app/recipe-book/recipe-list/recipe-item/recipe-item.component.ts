bd7435e671ced0844a59c3ba37dcb6c3a6975b23
Casey <casey.gardonio@foat.org>
2021-01-04 15:59:13 -0600

    Section 12 part 163"

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 // import { RecipeService } from '../../recipe.service'
 
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -9,10 +10,11 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  constructor(
-    // private recipeService: RecipeService
-    ) { }
+  // constructor(
+  //   private recipeService: RecipeService
+  // ) { }
 
   ngOnInit(): void {
   }

88e684fe27fb6594ffbf324b4160797e2945e0ea
Casey <casey.gardonio@foat.org>
2021-02-01 16:24:53 -0600

    completed through section 20 part 299

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 
@@ -7,11 +9,18 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   // @Output() featureSelected = new EventEmitter<string>(); 
-  constructor(private dataStorageService: DataStorageService) { }
+  userSub = new Subscription;
+  isAuthenticated = false;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true
+      //!user = ? false : true can also be writte as !!user
+    });
   }
 
   // onSelect(feature: string) {
@@ -24,5 +33,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchRecipes() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }

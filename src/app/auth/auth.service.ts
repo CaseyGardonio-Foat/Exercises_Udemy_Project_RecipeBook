a0dff0327831f9947d2d60f4947665098927cdb1
Casey <casey.gardonio@foat.org>
2021-02-02 16:23:12 -0600

    completed through section 20 part 304; user.token still not recognized

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -87,11 +88,24 @@ export class AuthService {
     if(loadedUser.token) {
       this.user.next(loadedUser);
     }
+    const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+    this.autoLogout(expirationDuration);
   }
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.clear();
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(()=> {
+      this.logout();
+    }, expirationDuration)
   }
 
   private handleAuthentication(
@@ -103,6 +117,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
   }
 
   private handleError(errorRes: HttpErrorResponse) {

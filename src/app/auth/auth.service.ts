85a567c1057d92e520087869d84039c476bd6096
Casey <casey.gardonio@foat.org>
2021-02-03 16:55:17 -0600

    adjusted code for logout method in auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -95,7 +95,7 @@ export class AuthService {
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
-    localStorage.clear();
+    localStorage.removeItem('userData');
     if(this.tokenExpirationTimer){
       clearTimeout(this.tokenExpirationTimer);
     }

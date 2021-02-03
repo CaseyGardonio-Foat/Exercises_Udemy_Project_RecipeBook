76e4ab97a7f57f464d54b8e43fc996af0d86c545
Casey <casey.gardonio@foat.org>
2021-02-03 15:33:47 -0600

    completed through section 21 part 311

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -10,9 +10,13 @@ export class AlertComponent implements OnInit {
 
   constructor() { }
   @Input() message: string;
-
+  @Output() close = new EventEmitter<void>();
 
   ngOnInit(): void {
+  }
+
+  onClose() {
+    this.close.emit();
   }
 
 }
